0634583dcf88e3e05393f2a64442d4415847789a
Oladipo Omotosho <65185499+Urahara-Kenpachi@users.noreply.github.com>
2022-10-19 15:10:17 +0100

    setting up RN env using expo and android studio

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,12 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: ["tailwindcss-react-native/babel"],
   };
 };
+
+// module.exports = {
+// - plugins: [],
+// + plugins: ["tailwindcss-react-native/babel"],
+// };
